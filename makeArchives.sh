c0ee8e0e55c362c9a32fa4a499363ddcfbcd919e
Alexander Galanin <al@galanin.nnov.ru>
2008-06-10 22:35:23 +0000

    added make archives script and changelog

---
+++ makeArchives.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+dir=`mktemp -d`
+pwd=`pwd`
+
+# make program tarball
+id="fuse-zip-`basename "$pwd"`"
+tmp="$dir/$id"
+
+mkdir "$tmp"
+cp -t "$tmp" *.cpp *.h Makefile INSTALL LICENSE README changelog
+
+rm -f "$id.tar.bz2"
+cd "$dir"
+tar -cvjf "$pwd/$id.tar.bz2" "$id"
+cd "$pwd"
+
+# make tests tarball
+id="fuse-zip-tests-r`svn info tests | grep Revision | cut -d \  -f 2`"
+tmp="$dir/$id"
+
+mkdir "$tmp"
+mkdir "$tmp/kio_copy"
+cp -t "$tmp" tests/README tests/run-tests.tcl tests/unpackfs.config
+cp -t "$tmp/kio_copy" tests/kio_copy/kio_copy.pro tests/kio_copy/main.cpp
+
+rm -rf "$id.tar.bz2"
+cd "$dir"
+tar -cvjf "$pwd/$id.tar.bz2" "$id"
+cd "$pwd"
+
+rm -rf "$dir"
+
