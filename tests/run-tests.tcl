048dbcd56a6fba039887bfec8c4d1224536b3dae
Alexander Galanin <al@galanin.nnov.ru>
2008-06-10 00:51:04 +0000

    more accurate time counting for fuse-zip, added new tests, fixed mc-uzip zip-* tests behaviour, changed zip-* tests data to bring fuse-zip to win :)

--- tests/run-tests.tcl
+++ tests/run-tests.tcl
@@ -41,14 +41,17 @@ set tests {
     zip-mixed       "Compress files generated from /dev/{urandom,zero}"
     unzip-mixed     "Uncompress files generated from /dev/{urandom,zero}"
 
+    add-small-small "Add small file to small archive"
+    add-small-big   "Add small file to big archive"
+    add-big-small   "Add big file to small archive"
+    add-big-big     "Add big file to big archive"
+
     zip-linuxsrc    "Compress linux kernel sources"
     unzip-linuxsrc  "Uncompress linux kernel sources"
 }
 
-#debug
+##debug
 #set tests {
-#    zip-linuxsrc    "compress linux sources"
-#    unzip-linuxsrc    "compress linux sources"
 #}
 
 ###############################################################################
@@ -58,18 +61,15 @@ set tests {
 proc prepareRandomData {args} {
     global extractDir
 
-    set size 50
+    set size 2
+    set count 10
 
     file mkdir $extractDir/data
     foreach type $args {
-        exec dd if=/dev/$type of=$extractDir/data/$type bs=1M count=$size > /dev/null 2> /dev/null
+        for {set i 0} {$i < $count} {incr i} {
+            exec dd if=/dev/$type of=$extractDir/data/$type$i bs=1M count=$size > /dev/null 2> /dev/null
+        }
     }
-}
-
-proc cleanRandomData {} {
-    global extractDir
-
-    file delete -force $extractDir/data
 }
 
 proc prepareArchive {dir file} {
@@ -81,10 +81,19 @@ proc prepareArchive {dir file} {
     cd $w
 }
 
+proc makeFile {name size} {
+    global extractDir
+    array set sizes {
+        small   1
+        big     20
+    }
+    exec dd if=/dev/zero of=$extractDir/$name bs=1M count=$sizes($size) > /dev/null 2> /dev/null
+}
+
 proc prepareData {action} {
     puts "Generating data... "
     puts -nonewline "Generating data... "
-    switch -exact $action {
+    switch -glob $action {
         zip-urandom {
             prepareRandomData urandom
         }
@@ -97,23 +106,32 @@ proc prepareData {action} {
         unzip-urandom {
             prepareRandomData urandom
             prepareArchive $::extractDir data
-            cleanRandomData
+            file delete -force $::extractDir/data
         }
         unzip-zero {
             prepareRandomData zero
             prepareArchive $::extractDir data
-            cleanRandomData
+            file delete -force $::extractDir/data
         }
         unzip-mixed {
             prepareRandomData urandom zero
             prepareArchive $::extractDir data
-            cleanRandomData
+            file delete -force $::extractDir/data
         }
         zip-linuxsrc {
             exec cp -r -t $::extractDir $::linuxSources
         }
         unzip-linuxsrc {
             prepareArchive $::linuxSources .
+        }
+        add-* {
+            set a [ lrange [ split $action "-" ] 1 2 ]
+            file mkdir $::extractDir/data
+            makeFile data/content [ lindex $a 0 ]
+            makeFile file [ lindex $a 1 ]
+            prepareArchive $::extractDir data
+            file delete -force $::extractDir/data
+            exec cp $::archive $::archive.copy
         }
     }
     puts "OK"
@@ -122,11 +140,15 @@ proc prepareData {action} {
 proc cleanData {action} {
     global archive extractDir
 
-    switch -exact $action {
-        zip-urandom -
-        zip-zero -
-        zip-mixed {
-            cleanRandomData
+    switch -glob $action {
+        zip-* {
+            file delete -force $extractDir
+            file mkdir $extractDir
+        }
+        add-* {
+            file delete $::archive.copy
+            file delete -force $extractDir
+            file mkdir $extractDir
         }
     }
 }
@@ -141,6 +163,9 @@ proc cleanTestData {action} {
         unzip-* {
             file delete -force $extractDir
             file mkdir $extractDir
+        }
+        add-* {
+            exec cp $::archive.copy $::archive
         }
     }
 }
@@ -170,6 +195,7 @@ proc fusezipExec {cmd} {
         $fuseZip $archive $mountPoint;\
         $cmd;\
         $fusermount -uz $mountPoint;\
+        while \[ -n `pgrep fuse-zip`\"\" \];do sleep 0.1s;done;\
     " ]
     checkArchive
     return $res
@@ -179,6 +205,7 @@ proc fuse-zip {action} {
     global mountPoint extractDir linuxSources
 
     switch -glob $action {
+        add-* -
         zip-* {
             return [ fusezipExec "cp -r -t $mountPoint $extractDir" ]
         }
@@ -195,6 +222,7 @@ proc kio-zip {action} {
     global archive extractDir kioCopy
 
     switch -glob $action {
+        add-* -
         zip-* {
             return "n/a n/a n/a"
         }
@@ -211,14 +239,15 @@ proc mc-uzip {action} {
     global zip uzip archive extractDir
 
     switch -glob $action {
+        add-* -
         zip-* {
             # mc uzip cannot handle empty files
             exec $zip $archive $::argv0
             return [ timeExec sh -c "\
                 find $extractDir -type d | cut -b 2- | tail -n +2 | while read f;\
                 do $uzip mkdir $archive \$f; done;\
-                find $extractDir -type d | cut -b 2- | while read f;\
-                do $uzip mkdir $archive \$f /\$f; done;\
+                find $extractDir -type f | cut -b 2- | while read f;\
+                do $uzip copyin $archive \$f /\$f; done;\
             " ]
         }
         unzip-* {
@@ -264,6 +293,7 @@ if [ catch {
             cleanTestData $t
         }
         cleanData $t
+        exec sync
         lappend results $t $l $res
     }
 } err ] {
