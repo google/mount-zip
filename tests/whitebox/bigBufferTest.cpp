08f6cfc2df801ef9bcd73414f23d3fca8c6b0dba
Alexander Galanin <al@galanin.nnov.ru>
2010-02-13 08:37:52 +0300

    bigBufferTest splitted to functions
    --HG--
    branch : automated_tests

--- tests/whitebox/bigBufferTest.cpp
+++ tests/whitebox/bigBufferTest.cpp
@@ -22,8 +22,8 @@ struct zip *zip_open(const char *, int, int *errorp) {
     return NULL;
 }
 
-int zip_error_to_str(char *buf, size_t len, int, int) {
-    return strncpy(buf, "Expected error", len) - buf;
+int zip_error_to_str(char *, size_t, int, int) {
+    return 0;
 }
 
 // only stubs
@@ -97,11 +97,11 @@ const char *zip_strerror(struct zip *) {
     return NULL;
 }
 
-// test functions
+////////////////////////////////////////////////////////////////////////////
+// TESTS
+////////////////////////////////////////////////////////////////////////////
 
-int main(int, char **) {
-    initTest();
-
+void chunkLocators() {
     // static functions test
     assert(BigBuffer::chunksCount(0) == 0);
     assert(BigBuffer::chunksCount(1) == 1);
@@ -123,159 +123,168 @@ int main(int, char **) {
     assert(BigBuffer::chunkOffset(BigBuffer::chunkSize - 1) == BigBuffer::chunkSize - 1);
     assert(BigBuffer::chunkOffset(BigBuffer::chunkSize + 1) == 1);
     assert(BigBuffer::chunkOffset(BigBuffer::chunkSize * 2 - 1) == BigBuffer::chunkSize - 1);
+}
 
-    // create-delete
-    {
-        BigBuffer bb;
-        assert(bb.len == 0);
-    }
+void createDelete() {
+    BigBuffer bb;
+    assert(bb.len == 0);
+}
 
-    // truncation
-    {
-        BigBuffer bb;
+void truncate() {
+    BigBuffer bb;
 
-        bb.truncate(22);
-        assert(bb.len == 22);
+    bb.truncate(22);
+    assert(bb.len == 22);
 
-        bb.truncate(2);
-        assert(bb.len == 2);
+    bb.truncate(2);
+    assert(bb.len == 2);
 
-        bb.truncate(BigBuffer::chunkSize);
-        assert(bb.len == BigBuffer::chunkSize);
+    bb.truncate(BigBuffer::chunkSize);
+    assert(bb.len == BigBuffer::chunkSize);
 
-        bb.truncate(BigBuffer::chunkSize + 1);
-        assert(bb.len == BigBuffer::chunkSize + 1);
+    bb.truncate(BigBuffer::chunkSize + 1);
+    assert(bb.len == BigBuffer::chunkSize + 1);
 
-        bb.truncate(0);
-        assert(bb.len == 0);
-    }
+    bb.truncate(0);
+    assert(bb.len == 0);
+}
 
-    // read
-    {
-        char buf[0xff];
-        char empty[0xff];
-        memset(empty, 0, 0xff);
-        int nr;
-        BigBuffer bb;
+void readFile() {
+    char buf[0xff];
+    char empty[0xff];
+    memset(empty, 0, 0xff);
+    int nr;
+    BigBuffer bb;
 
-        nr = bb.read(buf, 100, 0);
-        assert(nr == 0);
+    nr = bb.read(buf, 100, 0);
+    assert(nr == 0);
 
-        nr = bb.read(buf, 100, 100);
-        assert(nr == 0);
+    nr = bb.read(buf, 100, 100);
+    assert(nr == 0);
 
-        bb.truncate(10);
-        nr = bb.read(buf, 10, 0);
-        assert(nr == 10);
-        assert(memcmp(buf, empty, nr) == 0);
+    bb.truncate(10);
+    nr = bb.read(buf, 10, 0);
+    assert(nr == 10);
+    assert(memcmp(buf, empty, nr) == 0);
 
-        bb.truncate(BigBuffer::chunkSize);
-        nr = bb.read(buf, 10, BigBuffer::chunkSize - 5);
-        assert(nr == 5);
-        assert(memcmp(buf, empty, nr) == 0);
-    }
+    bb.truncate(BigBuffer::chunkSize);
+    nr = bb.read(buf, 10, BigBuffer::chunkSize - 5);
+    assert(nr == 5);
+    assert(memcmp(buf, empty, nr) == 0);
+}
 
-    // read (size > chunkSize)
-    {
-        int n = BigBuffer::chunkSize * 3 + 15;
-        char buf[n];
-        char empty[n];
-        memset(empty, 0, n);
-        int nr;
-        BigBuffer bb;
+// read (size > chunkSize)
+void readFileOverChunkSize() {
+    int n = BigBuffer::chunkSize * 3 + 15;
+    char buf[n];
+    char empty[n];
+    memset(empty, 0, n);
+    int nr;
+    BigBuffer bb;
 
-        nr = bb.read(buf, n, 0);
-        assert(nr == 0);
+    nr = bb.read(buf, n, 0);
+    assert(nr == 0);
 
-        nr = bb.read(buf, n, 100);
-        assert(nr == 0);
+    nr = bb.read(buf, n, 100);
+    assert(nr == 0);
 
-        bb.truncate(10);
-        nr = bb.read(buf, 10, 0);
-        assert(nr == 10);
-        assert(memcmp(buf, empty, nr) == 0);
+    bb.truncate(10);
+    nr = bb.read(buf, 10, 0);
+    assert(nr == 10);
+    assert(memcmp(buf, empty, nr) == 0);
 
-        bb.truncate(BigBuffer::chunkSize);
-        nr = bb.read(buf, n, BigBuffer::chunkSize - 5);
-        assert(nr == 5);
-        assert(memcmp(buf, empty, nr) == 0);
+    bb.truncate(BigBuffer::chunkSize);
+    nr = bb.read(buf, n, BigBuffer::chunkSize - 5);
+    assert(nr == 5);
+    assert(memcmp(buf, empty, nr) == 0);
 
-        bb.truncate(BigBuffer::chunkSize * 2 - 12);
-        nr = bb.read(buf, n, 1);
-        assert(nr == BigBuffer::chunkSize * 2 - 12 - 1);
-        assert(memcmp(buf, empty, nr) == 0);
+    bb.truncate(BigBuffer::chunkSize * 2 - 12);
+    nr = bb.read(buf, n, 1);
+    assert(nr == BigBuffer::chunkSize * 2 - 12 - 1);
+    assert(memcmp(buf, empty, nr) == 0);
 
-        bb.truncate(BigBuffer::chunkSize * 10);
-        nr = bb.read(buf, n, 1);
-        assert(nr == n);
-        assert(memcmp(buf, empty, nr) == 0);
-    }
+    bb.truncate(BigBuffer::chunkSize * 10);
+    nr = bb.read(buf, n, 1);
+    assert(nr == n);
+    assert(memcmp(buf, empty, nr) == 0);
+}
 
-    // read data created by truncate
-    {
-        char buf[BigBuffer::chunkSize];
-        char empty[BigBuffer::chunkSize];
-        memset(empty, 0, BigBuffer::chunkSize);
-        BigBuffer b;
-        b.truncate(BigBuffer::chunkSize);
-        assert(b.len == BigBuffer::chunkSize);
-        int nr = b.read(buf, BigBuffer::chunkSize, 0);
-        assert((unsigned)nr == BigBuffer::chunkSize);
-        assert(memcmp(buf, empty, BigBuffer::chunkSize) == 0);
-    }
+// read data created by truncate
+void truncateRead() {
+    char buf[BigBuffer::chunkSize];
+    char empty[BigBuffer::chunkSize];
+    memset(empty, 0, BigBuffer::chunkSize);
+    BigBuffer b;
+    b.truncate(BigBuffer::chunkSize);
+    assert(b.len == BigBuffer::chunkSize);
+    int nr = b.read(buf, BigBuffer::chunkSize, 0);
+    assert((unsigned)nr == BigBuffer::chunkSize);
+    assert(memcmp(buf, empty, BigBuffer::chunkSize) == 0);
+}
 
-    // writing to file
-    {
-        char buf[0xff];
-        char buf2[0xff];
-        int nr, nw;
-        BigBuffer bb;
+// writing to file
+void writeFile() {
+    char buf[0xff];
+    char buf2[0xff];
+    int nr, nw;
+    BigBuffer bb;
 
-        nw = bb.write(buf, 0, 0);
-        assert(nw == 0);
-        assert(bb.len == 0);
+    nw = bb.write(buf, 0, 0);
+    assert(nw == 0);
+    assert(bb.len == 0);
 
-        memset(buf, 1, 10);
-        memset(buf+10, 2, 10);
-        nw = bb.write(buf, 20, 0);
-        assert(nw == 20);
-        assert(bb.len == 20);
-        nr = bb.read(buf2, 30, 0);
-        assert(nr == 20);
-        assert(memcmp(buf, buf2, 20) == 0);
+    memset(buf, 1, 10);
+    memset(buf+10, 2, 10);
+    nw = bb.write(buf, 20, 0);
+    assert(nw == 20);
+    assert(bb.len == 20);
+    nr = bb.read(buf2, 30, 0);
+    assert(nr == 20);
+    assert(memcmp(buf, buf2, 20) == 0);
 
-        bb.truncate(0);
-        nw = bb.write(buf, 20, 0);
-        assert(nw == 20);
-        assert(bb.len == 20);
-        nr = bb.read(buf2, 20, 10);
-        assert(nr == 10);
-        assert(memcmp(buf + 10, buf2, 10) == 0);
-    }
+    bb.truncate(0);
+    nw = bb.write(buf, 20, 0);
+    assert(nw == 20);
+    assert(bb.len == 20);
+    nr = bb.read(buf2, 20, 10);
+    assert(nr == 10);
+    assert(memcmp(buf + 10, buf2, 10) == 0);
+}
 
-    // read data from file expanded by write
-    {
-        int n = BigBuffer::chunkSize * 2;
-        char buf[n];
-        char expected[n];
-        memset(expected, 0, n);
-        BigBuffer b;
+// read data from file expanded by write
+void readExpanded() {
+    int n = BigBuffer::chunkSize * 2;
+    char buf[n];
+    char expected[n];
+    memset(expected, 0, n);
+    BigBuffer b;
 
-        memset(buf, 'a', 10);
-        memset(expected, 'a', 10);
-        b.write(buf, 10, 0);
-        assert(b.len == 10);
+    memset(buf, 'a', 10);
+    memset(expected, 'a', 10);
+    b.write(buf, 10, 0);
+    assert(b.len == 10);
 
-        memset(buf, 'z', 10);
-        memset(expected + BigBuffer::chunkSize + 10, 'z', 10);
-        b.write(buf, 10, BigBuffer::chunkSize + 10);
-        assert(b.len == BigBuffer::chunkSize + 20);
+    memset(buf, 'z', 10);
+    memset(expected + BigBuffer::chunkSize + 10, 'z', 10);
+    b.write(buf, 10, BigBuffer::chunkSize + 10);
+    assert(b.len == BigBuffer::chunkSize + 20);
 
-        int nr = b.read(buf, n, 0);
-        assert((unsigned)nr == BigBuffer::chunkSize + 20);
-        assert(memcmp(buf, expected, nr) == 0);
-    }
+    int nr = b.read(buf, n, 0);
+    assert((unsigned)nr == BigBuffer::chunkSize + 20);
+    assert(memcmp(buf, expected, nr) == 0);
+}
 
+int main(int, char **) {
+    initTest();
+
+    chunkLocators();
+    createDelete();
+    truncate();
+    readFile();
+    readFileOverChunkSize();
+    truncateRead();
+    writeFile();
+    readExpanded();
     //TODO: read from zip, write to zip
 
     return EXIT_SUCCESS;
