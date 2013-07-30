2634eda4cead95e9ead5fd91debbbf6507f991e6
Alexander Galanin <al@galanin.nnov.ru>
2013-07-30 14:36:13 +0400

    Re-added ifdefs for MacOs (but not FreeBSD)

--- lib/fuse-zip.cpp
+++ lib/fuse-zip.cpp
@@ -426,11 +426,19 @@ int fusezip_utimens(const char *path, const struct timespec tv[2]) {
     return 0;
 }
 
+#if ( __APPLE_ && __FreeBSD__ >= 10 )
+int fusezip_setxattr(const char *, const char *, const char *, size_t, int, uint32_t) {
+#else
 int fusezip_setxattr(const char *, const char *, const char *, size_t, int) {
+#endif
     return -ENOTSUP;
 }
 
+#if ( __APPLE_ && __FreeBSD__ >= 10 )
+int fusezip_getxattr(const char *, const char *, char *, size_t, uint32_t) {
+#else
 int fusezip_getxattr(const char *, const char *, char *, size_t) {
+#endif
     return -ENOTSUP;
 }
 
