f9c4a1a37fc842d4f11f89e603682b465b20af88
Alexander Galanin <al@galanin.nnov.ru>
2019-03-12 20:56:15 +0300

    version 0.6.0

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////
-//  Copyright (C) 2008-2018 by Alexander Galanin                          //
+//  Copyright (C) 2008-2019 by Alexander Galanin                          //
 //  al@galanin.nnov.ru                                                    //
 //  http://galanin.nnov.ru/~al                                            //
 //                                                                        //
@@ -22,7 +22,7 @@
 
 #define FUSE_USE_VERSION 27
 #define PROGRAM "fuse-zip"
-#define VERSION "0.5.0"
+#define VERSION "0.6.0"
 
 #endif
 
